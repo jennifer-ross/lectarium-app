1624356309a14fd1131a408c5397dbbd61ce2531
jennifer23 <38167698+jennifer23@users.noreply.github.com>
2022-06-25 17:07:56 +0300

    Add utils

---
+++ lib/core/utils.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+import 'locator.dart';
+
+bool isEmpty(dynamic value) {
+  return ["", null, false, 0].contains(value);
+}
+
+void showGlobalError(String value) {
+  GlobalKey<ScaffoldMessengerState> scaffold =
+      locator.get<GlobalKey<ScaffoldMessengerState>>('scaffold');
+
+  scaffold.currentState?.activate();
+  scaffold.currentState?.showSnackBar(SnackBar(
+    backgroundColor: Colors.black,
+    content: Text(
+      value,
+      style: const TextStyle(color: Colors.redAccent),
+    ),
+    action: SnackBarAction(
+      textColor: Colors.white,
+      label: 'OK',
+      onPressed: () => hideGlobalError(),
+    ),
+  ));
+}
+
+void hideGlobalError() {
+  GlobalKey<ScaffoldMessengerState> scaffold =
+      locator.get<GlobalKey<ScaffoldMessengerState>>('scaffold');
+
+  scaffold.currentState?.hideCurrentSnackBar();
+  scaffold.currentState?.deactivate();
+}
