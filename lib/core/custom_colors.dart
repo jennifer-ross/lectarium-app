b89d7264e0d548c6b34bbea2998d426ae6522a42
jennifer23 <38167698+jennifer23@users.noreply.github.com>
2022-07-05 09:38:13 +0300

    Custom colors UI

---
+++ lib/core/custom_colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+extension CustomColors on Colors {
+  static const Color authCardBackground = Color(0xFFF2F7FD);
+  static const Color authButtonBackground = Color(0xFF689BFB);
+  static const Color authFieldBackground = Color(0xFFE7EBEF);
+  static const Color authFieldLabelFocused = Color.fromARGB(255, 128, 128, 128);
+}
